976f208436528626c15144ee45c8b2b58ae52b7a
Dave <dave@blakey.co>
2022-06-03 13:53:11 +0200

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -4,7 +4,6 @@ module.exports = {
     { "name": "beta", "channel": "beta", "prerelease": "beta" },
   ],
   "plugins": [
-    "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/github"
   ]
